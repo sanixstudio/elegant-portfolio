7f7e52b7373133b1dbbff21e5fa7d5ba171cfd50
Adnan Niaz <sanixstudio@gmail.com>
2021-07-22 02:57:01 -0700

    created sticky menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,34 @@
 const toggleBtn = document.querySelector('#toggle-menu-bars');
 const nav = document.querySelector('.nav');
+const header = document.querySelector('header');
+const headerBarInner = document.querySelector('.header-bar-inner');
+const headerBar = document.querySelector('.header-bar');
+const stickyMenu = document.querySelector('.sticky-menu-container');
+const home = document.querySelector('#home');
 
-toggleBtn.onclick = ()=> {
+toggleBtn.onclick = () => {
   toggleBtn.classList.toggle('fa-times');
   nav.style.display !== "flex" ? nav.style.display = 'flex' : nav.style.display = 'none'
 }
+
+home.addEventListener('click', () => {
+  console.log("hi")
+})
+
+document.addEventListener('scroll',(e) => {
+  let HeaderHeight = header.offsetHeight;
+  // (scrollY >= HeaderHeight) ? stickyMenu.style.display = 'unset' : stickyMenu.style.display = 'none'
+  // (scrollY >= HeaderHeight) ? stickyMenu.style.transform = 'translateY(0)' : stickyMenu.style.transform = 'translateY(-100%)'
+  if (scrollY >= HeaderHeight) {
+    headerBar.style.position = 'fixed'
+    headerBar.style.width = '100%'
+    headerBar.style.padding = '0 1em'
+    headerBar.style.backgroundImage = 'linear-gradient(to right, #503EBC, #4F76D3)'
+    headerBarInner.style.maxWidth = '960px'
+    headerBarInner.style.margin = '0 auto'
+  } else {
+    headerBar.style.position = 'initial'
+    headerBar.style.backgroundImage = 'unset';
+    headerBar.style.padding = 'unset'
+  }
+})
